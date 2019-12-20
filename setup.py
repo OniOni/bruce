ee42fc4da67c2bf8fe44b284c1a75a9939f19469
mathieu <mathieu.c.sabourin@gmail.com>
2019-12-20 15:57:47 -0800

    [feat] Add upload task.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages, setup  # type: ignore
 
 setup(
-    name="bruce",
+    name="bruce-bld",
     version="0.0.1",
     description="Small Command Executor",
     author="Mathieu Sabourin",
